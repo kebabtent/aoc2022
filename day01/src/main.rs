ed64d52ef4656ff7509e74a6fb378ea29f6867b2
kebabtent <kebabtent@gmail.com>
2022-12-06 17:45:09 +0100

    day01: peek

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,16 +2,14 @@ use common::{read_all_lines, Buffer, IterExt};
 
 fn main() {
 	let b = read_all_lines()
+		.peekable()
 		.batching(|t| {
-			let s = t
-				.take_while(|l| !l.is_empty())
-				.filter_map(|l| l.parse::<u32>().ok())
-				.sum::<u32>();
-			if s > 0 {
-				Some(s)
-			} else {
-				None
-			}
+			t.peek()?;
+			Some(
+				t.take_while(|l| !l.is_empty())
+					.filter_map(|l| l.parse::<u32>().ok())
+					.sum::<u32>(),
+			)
 		})
 		.fold(Buffer::new(3), |mut b, v| {
 			b.insert_sorted(v);
