b04e5837a7e8c35c98f9f8c74b34e3f2a9f57c3e
kebabtent <kebabtent@gmail.com>
2022-12-06 17:10:24 +0100

    day02: shorten

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,39 +1,11 @@
 use common::{read_lines, Doublet, IterExt, TupleSum};
 
-#[derive(Clone, Copy, Debug)]
-struct L(u32);
-
-impl L {
-	fn c(self, o: Self) -> u32 {
-		(self.partial_cmp(&o).unwrap() as i8 * 3 + 4) as u32 + self.0
+fn c(l: u32, r: u32) -> u32 {
+	let mut c = l.partial_cmp(&r).unwrap();
+	if l + r == 2 {
+		c = c.reverse();
 	}
-
-	fn p(self, m: Self) -> Self {
-		Self((self.0 + m.0 + 2) % 3)
-	}
-}
-
-impl PartialEq for L {
-	fn eq(&self, o: &Self) -> bool {
-		self.partial_cmp(o) == Some(std::cmp::Ordering::Equal)
-	}
-}
-
-impl PartialOrd for L {
-	fn partial_cmp(&self, o: &Self) -> Option<std::cmp::Ordering> {
-		let (s, o) = (self.0, o.0);
-		if s + o == 2 {
-			o.partial_cmp(&s)
-		} else {
-			s.partial_cmp(&o)
-		}
-	}
-}
-
-impl From<char> for L {
-	fn from(v: char) -> Self {
-		Self((v as u32 - 65) % 23)
-	}
+	(c as i8 * 3 + 4) as u32 + l
 }
 
 fn main() {
@@ -41,10 +13,10 @@ fn main() {
 		.filter_map(|l| {
 			l.chars()
 				.filter(|&c| c != ' ')
-				.map(|c| L::from(c))
+				.map(|c| (c as u32 - 65) % 23)
 				.next_tuple::<Doublet<_>>()
 		})
-		.map(|(a, b)| (b.c(a), a.p(b).c(a)))
+		.map(|(a, b)| (c(b, a), c((a + b + 2) % 3, a)))
 		.tuple_sum();
 	println!("{a}");
 	println!("{b}");
