4c4427b505fb8eec66939b84896cd0c9fd9ec700
kebabtent <kebabtent@gmail.com>
2022-12-06 19:02:36 +0100

    day04: shorten

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,17 +1,16 @@
 use common::{read_lines, IterExt, Quartet, TupleSum};
+use std::cmp::{max, min};
 
 fn main() {
 	let (a, b) = read_lines()
 		.filter_map(|l| {
 			l.split(|c| c == '-' || c == ',')
 				.filter_map(|v| v.parse().ok())
-				.next_tuple::<Quartet<u32>>()
+				.next_tuple::<Quartet<i32>>()
 		})
 		.map(|(a, b, c, d)| {
-			(
-				(a >= c && b <= d) || (c >= a && d <= b),
-				(a >= c && a <= d) || (c >= a && c <= b),
-			)
+			let o = min(b, d) - max(a, c);
+			(o == min(b - a, d - c), o >= 0)
 		})
 		.tuple_sum();
 	println!("{a}");
